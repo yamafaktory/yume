ca19e368a4cc7771fa728d1bc2c341d5812ab30d
yamafaktory <yamafaktory@gmail.com>
2020-05-30 23:15:55 +0200

    chore: fix clippy hint

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 pub const BUFFER_SIZE: usize = 4096;
 pub const CLIENT_PORT: i32 = 3000;
-pub const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
+pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 pub const NONCE_LENGTH: usize = 12;
 pub const SERVER_PORT: i32 = 3001;
 pub const TIMEOUT: u64 = 5;
-pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+pub const VERSION: &`str = env!("CARGO_PKG_VERSION");
